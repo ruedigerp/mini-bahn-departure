9215d5532012849dac0bbef7b4c9b748082f74c1
Rüdiger Küpper <ruediger@kuepper.nrw>
2018-04-26 09:03:00 +0200

    config.py delete in gitignore

---
+++ config.py
@@ -0,0 +1,10 @@
+locations = ['Düsseldorf/Zoo', 'Oberhausen/Hauptbahnhof', 'Essen/Am Kreyenkrop', 'Essen/Hauptbahnhof',
+'Essen/Borbeck', 'Duisburg/Hauptbahnhof', 'Düsseldorf/Flughafen Bahnhof'
+]
+remote_api = 'https://vrrf.finalrewind.org/'
+remote_api_params = 'frontend=json'
+
+# locations = ['City/Station', 'City2/Station2']
+# # remote_api = 'http://localhost:4000/'
+# remote_api = 'http://openservice-test.vrr.de/static02/XML_STOPFINDER_REQUEST?sessionID=0&requestID=0&language=DE'
+# remote_api_params = 'output=json'
